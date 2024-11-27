ad75a45b0a84ee923c9e144a2b50a1800a186b47
Dilan Dilruksha <100220079+dilrukshax@users.noreply.github.com>
2024-11-27 21:34:32 +0530

    Create authentication and authorization part

---
+++ BlazorApp/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorApp.Services
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity());
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}
